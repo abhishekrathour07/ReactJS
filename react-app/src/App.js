5185a81f1c93a3c8036f6abf195a671ee9e7ae67
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-08-30 02:40:26 +0530

    modified data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="demo">
+        <nav>
+          <ul>
+            <li>home</li>
+            <li>contact</li>
+            <li>about</li>
+            <li>support</li>
+          </ul>
+
+
+        </nav>
+      </div>
+      <div className="para">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores blanditiis iusto, sunt consectetur magnam quae tenetur modi illum magni similique ex doloribus nulla saepe laboriosam! Ad distinctio ipsum corporis! Exercitationem, sapiente eius ut commodi eveniet non labore enim earum qui.</p>
+        <img src={logo} alt="react logo" height='150px' align='center' />
+      </div>
+    </>
   );
 }
 
