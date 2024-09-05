82a92d21e9c67dcb3f64be740dec53901474aa79
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-09-05 23:59:07 +0530

    react app3

---
+++ react-app3/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+        <div className="navbar">
+            <div className="web-name">
+              <p>Mushroom</p></div>
+            <button className="waitlist">join waitlist</button>
+        </div>
+    </>
+  )
+}
